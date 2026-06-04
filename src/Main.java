f7184a11a63d8fb15302f9fb7ae423c590d2ccd3
Karina Reyes <reyesaguilark@gmail.com>
2026-06-03 22:26:19 -0600

    inicio modulo nuevo

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola, Mundo!");
         //vamos a iniciar el reto 5 primer commit
+         System.out.println("Hola, Mundo iniciamos nuevo modulo!");
     }
 }
